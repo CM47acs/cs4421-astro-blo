d308a87f0d87e802f3f5f2a630779c07bd572f82
ciaran <mciaran47@gmail.com>
2026-09-09 10:32:22 +0100

    catmod3 fix, days

--- gity.py
+++ gity.py
@@ -1,13 +1,21 @@
 # testing
-cat_status = "Happy happy happpy"
+import random
 
-cat = True
-print(cat)
-if cat: True 
+
+day = random.randint(1,8)
+
+if day > 4.1:
+    cat = True
+else:
+    cat = False
+if cat: 
+    cat_status = "meow"
+else:
+    cat_status = ""
 print(cat_status)
 
 if cat:
      dog_status = "bark"
 else: 
-     dog_status = "wag"
+     dog_status = "ZZZ"
 print(dog_status)
